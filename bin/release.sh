83628d12a3a9ce540b43b3cc1352fc82f5fdd9fd
christina <christina.inching.triceps@gmail.com>
2020-04-05 21:05:35 +0900

    change build script

--- bin/release.sh
+++ bin/release.sh
@@ -4,7 +4,14 @@ angularDir=./src/github-pages
 
 build_release() {
   cd $angularDir
+
+  if type "コマンド" > /dev/null 2>&1; then
+    yarn global add @angular/cli
+  fi
+
+  yarn install
   yarn run build
+
   cd ../..
   cp -pr $angularDir/dist/github-pages/* .
 }
