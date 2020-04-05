28f4f2ffed33b1786826fa2b7696ad5feb7a4bb3
christina <christina.inching.triceps@gmail.com>
2020-04-06 04:09:09 +0900

    remove compiled files and update release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -3,16 +3,24 @@
 angularDir=./src/github-pages
 
 build_release() {
+  # buildのためにangularプロジェクトフォルダへ移動
   cd $angularDir
 
+  # buildに必要なものを用意
   if type "コマンド" > /dev/null 2>&1; then
     yarn global add @angular/cli
   fi
-
   yarn install
+
+  # build
   yarn run build  --prod
 
+  # 元のフォルダ(プロジェクトルート)に戻る
   cd ../..
+  # 過去にアップロードされたファイルを削除
+  rm main-es* polyfills-es* runtime-es* styles-es* vendor-es* styles.* 3rdpartylicenses.txt index.html favicon.ico
+
+  # ファイル強制配置
   \cp -pfr $angularDir/dist/github-pages/* .
 }
 
