743db30bbe538cba07f7155fb93dc6284e691dd5
christina <christina.inching.triceps@gmail.com>
2020-05-04 17:26:47 +0900

    change release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -7,18 +7,18 @@ build_release() {
   cd $angularDir
 
   # buildに必要なものを用意
-  if type "コマンド" > /dev/null 2>&1; then
+  if type "ng" > /dev/null 2>&1; then
     yarn global add @angular/cli
   fi
   yarn install
 
   # build
-  yarn run build  --prod
+  yarn run build  --prod --output-hashing=none
 
   # 元のフォルダ(プロジェクトルート)に戻る
-  cd ../..
+  cd -
   # 過去にアップロードされたファイルを削除
-  rm main-es* polyfills-es* runtime-es* styles-es* vendor-es* styles.* 3rdpartylicenses.txt index.html favicon.ico
+  rm main-es* polyfills-es* runtime-es* styles.* 3rdpartylicenses.txt index.html favicon.ico
 
   # ファイル強制配置
   \cp -pfr $angularDir/dist/github-pages/* .
